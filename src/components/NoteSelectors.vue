33a7b25db1e9ddd3a598a6a26faa86a29bece5c9
Erin Weston <westonerin1@gmail.com>
2020-12-21 20:47:47 -0500

    add NoteSelectors.vue and NoteSelector1.vue

---
+++ src/components/NoteSelectors.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="note-selectors">
+        <NoteSelector1 />
+        <div class="note-selector">
+          <p class="note-selector-title">Second note...</p>
+          <p class="note-selector-timestamp">Timestamp here...</p>
+        </div>
+        <div class="note-selector">
+          <p class="note-selector-title">Third note...</p>
+          <p class="note-selector-timestamp">Timestamp here...</p>
+        </div>
+      </div>
+</template>
+
+<script>
+import NoteSelector1 from "./NoteSelector1.vue";
+export default {
+  components: {
+    NoteSelector1,
+  },
+};
+</script>
